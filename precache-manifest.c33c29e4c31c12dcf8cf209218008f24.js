56ad57241fffea1f53afc01b81117337ab9f2e8e
Harsh <mangoharsh>
2019-12-09 11:04:48 +0530

    Updates

---
+++ precache-manifest.c33c29e4c31c12dcf8cf209218008f24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66592e728235e4fa3a912f99b539110",
+    "url": "/react-test-react-deploy/index.html"
+  },
+  {
+    "revision": "169d5aa48d0aa79d45b1",
+    "url": "/react-test-react-deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1032da8c71350a6bb489",
+    "url": "/react-test-react-deploy/static/js/2.f223fb61.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-test-react-deploy/static/js/2.f223fb61.chunk.js.LICENSE"
+  },
+  {
+    "revision": "169d5aa48d0aa79d45b1",
+    "url": "/react-test-react-deploy/static/js/main.f98ee5ba.chunk.js"
+  },
+  {
+    "revision": "cd386c57cd8854687ac5",
+    "url": "/react-test-react-deploy/static/js/runtime-main.cb79bd68.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test-react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
